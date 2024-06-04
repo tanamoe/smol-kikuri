2b7d9c455742a913028ad6003c65e05b63bc404a
Nguyen Do <khoanguyen.do@outlook.com>
2024-06-04 09:41:22 +0700

    build: add nix develop environment

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A Nix-flake-based Node.js development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    overlays = [
+      (final: prev: rec {
+        nodejs = prev.nodejs_latest;
+      })
+    ];
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit overlays system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [traceroute dig node2nix nodejs bun ffmpeg];
+      };
+    });
+  };
+}
