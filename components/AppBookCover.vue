e46f5be3430da62df0c8d35ef6829238ab82fd0a
Nguyen Do <khoanguyen.do@outlook.com>
2024-08-11 02:39:00 +0700

    feat: upstream sync

---
+++ components/AppBookCover.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { MetadataImages } from "@/types/common";
+
+defineProps<{
+  name?: string;
+  src?: string;
+  srcset?: MetadataImages | null;
+}>();
+</script>
+
+<template>
+  <AppImage
+    v-if="src"
+    class="aspect-[2/3] h-full w-full object-cover"
+    :src="src"
+    :srcset="srcset"
+    :alt="name"
+  />
+  <div
+    v-else
+    :class="[
+      'flex h-full w-full items-center justify-center text-center',
+      'bg-gray-200 text-gray-500',
+      'dark:bg-gray-800 dark:text-gray-400',
+      'font-condensed font-bold',
+      'p-6',
+      'aspect-[2/3]',
+      'overflow-hidden',
+    ]"
+  >
+    <span v-if="name">{{ name }}</span>
+    <span v-else>{{ $t("general.tba") }}</span>
+  </div>
+</template>
